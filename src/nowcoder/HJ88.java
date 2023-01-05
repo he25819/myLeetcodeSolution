1a61c76f465caf1606b44a07359a66ea72f536b4
xinweihe <he25819@163.com>
2023-01-05 15:59:48 +0800

    6 problems

---
+++ src/nowcoder/HJ88.java
@@ -0,0 +1,44 @@
+package nowcoder;
+
+import java.util.Scanner;
+
+public class HJ88 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String poker = "3 4 5 6 7 8 9 10 J Q K A 2 joker JOKER";
+        while (in.hasNextLine()) {
+            String s = in.nextLine();
+            String[] hands = s.split("-");
+            if (hands.length != 2) {
+                continue;
+            }
+            String[] hand0 = hands[0].split(" ");
+            String[] hand1 = hands[1].split(" ");
+            if (hand0.length == 2 && hands[0].length() == 11)
+                System.out.println(hands[0]);
+            else if (hand1.length == 2 && hands[1].length() == 11)
+                System.out.println(hands[1]);
+            else {
+                if (hand0.length != 4 && hand1.length == 4) {
+                    String[] tempH = hand0;
+                    hand0 = hand1;
+                    hand1 = tempH;
+                    String temp = hands[0];
+                    hands[0] = hands[1];
+                    hands[1] = temp;
+                }
+                if (hand0.length == 4) {
+                    if (hand1.length == 4) {
+                        System.out.println(poker.indexOf(hand0[0]) < poker.indexOf(hand1[0]) ? hands[1] : hands[0]);
+                    } else {
+                        System.out.println(hands[0]);
+                    }
+                } else if (hand0.length != hand1.length) {
+                    System.out.println("ERROR");
+                } else {
+                    System.out.println(poker.indexOf(hand0[0]) < poker.indexOf(hand1[0]) ? hands[1] : hands[0]);
+                }
+            }
+        }
+    }
+}
---
+++ src/nowcoder/HJ92.java
@@ -0,0 +1,46 @@
+package nowcoder;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class HJ92 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNextLine()) {
+            String str = in.nextLine().trim();
+            if (str.length() == 0)
+                continue;
+
+            boolean beforeNum = false;
+            int maxLen = 0;
+            int leftIndex = 0;
+            List<int[]> rst = new ArrayList<>();
+            for (int i = 0; i <= str.length(); i++) {
+                if (i != str.length() && str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+                    if (!beforeNum) {
+                        beforeNum = true;
+                        leftIndex = i;
+                    }
+                } else {
+                    if (beforeNum) {
+                        beforeNum = false;
+                        if (maxLen < i - leftIndex) {
+                            maxLen = i - leftIndex;
+                            rst.clear();
+                            rst.add(new int[]{leftIndex, i});
+                        } else if (maxLen == i - leftIndex) {
+                            rst.add(new int[]{leftIndex, i});
+                        } else {
+
+                        }
+                    }
+                }
+            }
+            for (int[] idxs : rst) {
+                System.out.print(str.substring(idxs[0], idxs[1]));
+            }
+            System.out.printf(",%d\n", maxLen);
+        }
+    }
+}
